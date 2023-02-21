3659c63a430a78968e4882162341f57254d4b2f9
nl909 <125609654+nl909@users.noreply.github.com>
2023-02-21 16:36:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,12 @@ streamlit.text(fruityvice_response)
 
 #new Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 
 # normalize the text display
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
